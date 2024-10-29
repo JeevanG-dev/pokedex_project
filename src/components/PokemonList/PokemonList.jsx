aea8500cb2173fc5c438f7315d6d0fd40db04c85
Jeevan Gaudel <jatingaudel01@gmail.com>
2024-10-29 21:36:03 +1100

    added some of the style and added next and preivous button and making it functinal

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,73 +1,75 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import './pokemonList.css'
+import "./pokemonList.css";
 import Pokemon from "../Pokemon/Pokemon";
 
-function PokemonList(){
+function PokemonList() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
 
-const [pokemonList,setPokemonList] = useState([])
-const [isloading, setIsLoading] = useState(true)
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
 
 
-   async function downloadPokemon(){
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon') // we are getting the data of list of 20 pokemons
+const [nextUrl, setNextUrl] = useState('')
+const [preUrl,setPreUrl] = useState('')
+
+
+  async function downloadPokemon() {
+    const response = await axios.get(url); // we are getting the data of list of 20 pokemons
     console.log(response);
 
-    const pokemonResults = response.data.results //here we are filtering the list and getting the result value from the list
+    const pokemonResults = response.data.results; //here we are filtering the list and getting the result value from the list
 
-  const pokemonResultPromise =  pokemonResults.map((pokemon) => axios.get(pokemon.url)) // here we iterating the array of result and getting the pokemons detail url to get each pokemon details which creates the promises
+    const pokemonResultPromise = pokemonResults.map((pokemon) =>
+      axios.get(pokemon.url)
+    ); // here we iterating the array of result and getting the pokemons detail url to get each pokemon details which creates the promises
 
-  const pokemonData = await axios.all(pokemonResultPromise) // passing that promise to axios.all and when the promises gets resloved then it pass the data
+    const pokemonData = await axios.all(pokemonResultPromise); // passing that promise to axios.all and when the promises gets resloved then it pass the data
 
+    const res = pokemonData.map((poke) => {
+      const pokeList = poke.data;
 
-  const res = pokemonData.map((poke)=>{
-
-    const pokeList = poke.data
-
-    return {name : pokeList.name, 
-        image: (pokeList.sprites.other) ? pokeList.sprites.other.dream_world.front_default : pokeList.sprites.front_shiny,
+      return {
+        name: pokeList.name,
+        image: pokeList.sprites.other
+          ? pokeList.sprites.other.dream_world.front_default
+          : pokeList.sprites.front_shiny,
         types: pokeList.types,
-        id: pokeList.id
-    
-    
-    }
-       
+        id: pokeList.id,
+      };
+    });
+
+    console.log(res);
 
 
-  })
+setNextUrl(response.data.next)
 
-  console.log(res)
+setPreUrl(response.data.previous)
 
-  setPokemonList(res)
-    setIsLoading(false)
-    
-   }
+    setPokemonList(res);
+    setIsLoading(false);
+  }
 
-   
-    useEffect(()=>{
-        downloadPokemon()
-    },[]) 
+  useEffect(() => {
+    downloadPokemon();
+  }, [url]);
 
+  return (
+    <>
+      <div className="pokemon-list-wrapper">
+        <div className="pokemon-wrapper">
+          {isloading
+            ? " downling"
+            : pokemonList.map((p) => <Pokemon name={p.name} image={p.image} />)}
+        </div>
 
-
-    return (
-        <>
- <div className="pokemon-list-wrapper">
-
-    <div>Pokemon List</div>
-
-<div className="pokemon-wrapper">
-{(isloading) ? " downling" : 
-pokemonList.map((p) => <Pokemon name={p.name} image={p.image}/>) }
-
-</div>
-
-
- </div>
-        </>
-    )
-
-
+        <div className="controls">
+          <button disabled={preUrl == null} onClick={()=> setUrl(preUrl)} >Previous</button>
+          <button disabled={nextUrl == null} onClick={()=> setUrl(nextUrl)}>Next</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PokemonList;
